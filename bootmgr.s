d8c9f91edf3636643c68b2003036e87a7021c963
0x7C00 <137730237+royalbyte@users.noreply.github.com>
2026-03-08 22:58:06 -0300

    Update bootmgr.s

--- bootmgr.s
+++ bootmgr.s
@@ -1,1 +1,19 @@
-; Currently W.I.P
+.code16
+.section .text
+.globl _start
+
+_start:
+    xorw %ax, %ax
+    movw %ax, %ds
+
+    movb $0x0e, %ah
+    movb $'A', %al
+    int $0x10
+
+loop:
+    jmp loop
+
+.org 510
+.word 0xAA55
+
+# FUTURE IMPLEMENTATION TO JUMP TO KERNEL
